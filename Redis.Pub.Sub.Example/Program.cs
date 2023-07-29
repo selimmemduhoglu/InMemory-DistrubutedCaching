b9fdf8f6bc74610a667f6ebf64e7e4e97122be76
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2023-07-29 19:49:46 +0300

    added : messageAdded

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer _connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber= _connectionMultiplexer.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Mesaj : ");
+    string _message = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel",_message);
+}
---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer _connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = _connectionMultiplexer.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
